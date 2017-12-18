480169b74e9aa533c314363bc930577c260f11c0
shred22 <shreyas.dange22@gmail.com>
2017-12-18 09:45:19 +0530

    Date Parse Demo

---
+++ src/classes/DateParseDemo.java
@@ -0,0 +1,19 @@
+package classes;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateParseDemo {
+
+	public static void main(String[] args) throws ParseException {
+
+
+		Date date = new Date();
+		String str = "18/12/2017";
+		date = new SimpleDateFormat("dd/MM/yyyy").parse(str);
+		System.out.println(date);
+
+	}
+
+}
